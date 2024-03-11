1d0809835b4146441a2d9ca478127704ab34ce7a
icanbe444 <icanbe444@gmail.com>
2024-03-11 21:46:09 +0000

    Created the index page and separated the footer/header/nav inisde the include

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+                        <p>Ajoke Mi &copy; <?php echo date('Y'); ?></p>
+                </div>
---
+++ index.php
@@ -0,0 +1,30 @@
+
+        <?php require_once('config.php') ?>
+        <?php require_once( ROOT_PATH . '/includes/header.php') ?> 
+        <?php require_once('includes/header.php') ?>
+        <title>icanbe's Recipe Blog | Home </title>
+</head>
+<body>
+        <!-- container - wraps whole page -->
+        <div class="container">
+                <?php include( ROOT_PATH . '/includes/nav.php') ?>
+                <?php include( ROOT_PATH . '/includes/banner.php') ?>
+
+                <!-- Page content -->
+                <div class="content">
+                        <h2 class="content-title">Recent Recipe</h2>
+                        <hr> 
+                        <hr>
+            
+                                
+                
+
+                <!-- footer -->
+                <?php include( ROOT_PATH . '/includes/footer.php') ?>
+               
+                <!-- // footer -->
+
+        </div>
+  
+</body>
+</html>
